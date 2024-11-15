6e5576b0b3c9f933eb2422e326162c3e62f39415
Limbow <joacovas@hotmail.com>
2024-11-15 12:09:13 -0300

    identado orrecto

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+
+from enum import Enum
+from functools import wraps
+from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt
-from functools import wraps
-from datetime import datetime
-from enum import Enum
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'root'
